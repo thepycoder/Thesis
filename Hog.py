bd7a6004d479146fbf689ce1cafb8f8a307f3315
Victor <victor.sonck@gmail.com>
2017-12-13 11:02:55 +0100

    Initial Commit

---
+++ Hog.py
@@ -0,0 +1,37 @@
+from imutils.object_detection import non_max_suppression
+from imutils import resize
+import numpy as np
+import time
+import cv2
+
+
+image = "Testimages/test7.png"
+
+hog = cv2.HOGDescriptor()
+hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+
+
+# load the image and resize it to (1) reduce detection time
+# and (2) improve detection accuracy
+image = cv2.imread(image)
+image = resize(image, width=min(400, image.shape[1]))
+orig = image.copy()
+
+# detect people in the image
+start = time.time()
+(rects, weights) = hog.detectMultiScale(image, winStride=(4, 4), padding=(8, 8), scale=1.05)
+end = time.time()
+print("It took %s seconds." % (end - start))
+
+# apply non-maxima suppression to the bounding boxes using a
+# fairly large overlap threshold to try to maintain overlapping
+# boxes that are still people
+rects = np.array([[x, y, x + w, y + h] for (x, y, w, h) in rects])
+pick = non_max_suppression(rects, probs=None, overlapThresh=0.65)
+
+# draw the final bounding boxes
+for (xA, yA, xB, yB) in pick:
+    cv2.rectangle(image, (xA, yA), (xB, yB), (0, 255, 0), 2)
+
+cv2.imshow("After NMS", image)
+cv2.waitKey(0)
---
+++ hogVideo.py
@@ -0,0 +1,49 @@
+from imutils.object_detection import non_max_suppression
+from imutils import resize
+import numpy as np
+import time
+import cv2
+
+
+hog = cv2.HOGDescriptor()
+hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+
+cap = cv2.VideoCapture()
+vid = cap.open("../Footage/TestSeq1.mp4")
+frameCount = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+
+start = time.time()
+
+while True:
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+
+    if not ret:
+        break
+
+    frame = resize(frame, width=min(400, frame.shape[1]))
+    (rects, weights) = hog.detectMultiScale(frame, winStride=(4, 4), padding=(8, 8), scale=1.05)
+
+    # apply non-maxima suppression to the bounding boxes using a
+    # fairly large overlap threshold to try to maintain overlapping
+    # boxes that are still people
+    rects = np.array([[x, y, x + w, y + h] for (x, y, w, h) in rects])
+    pick = non_max_suppression(rects, probs=None, overlapThresh=0.65)
+
+    # draw the final bounding boxes
+    for (xA, yA, xB, yB) in pick:
+        cv2.rectangle(frame, (xA, yA), (xB, yB), (0, 255, 0), 2)
+
+    # Display the resulting frame
+    # cv2.imshow('frame', frame)
+    # if cv2.waitKey(1) & 0xFF == ord('q'):
+    #     break
+
+end = time.time()
+print("[INFO] it took %s seconds." % (end - start))
+print("[INFO] clip has %s frames" % frameCount)
+print("[INFO] that makes %s fps" % (frameCount / (end - start)))
+
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
